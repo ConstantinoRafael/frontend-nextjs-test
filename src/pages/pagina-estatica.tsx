53e26e441e858417667ba0cb5ab3fe716576b1e7
Rafael <rafael.constantino89@gmail.com>
2024-10-10 11:08:23 -0300

    feat: static page

--- src/pages/pagina-estatica.tsx
+++ src/pages/pagina-estatica.tsx
@@ -7,49 +7,59 @@
  * - A página deve ser atualizada a cada 1 minuto
  */
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import styles from '@/styles/lista.module.css';
-import { ICity } from '@/types/city.d';
+import styles from "@/styles/lista.module.css";
+import { ICity } from "@/types/city.d";
 
-export default function Lista() {
-	const [list, setUsers] = useState<Array<ICity>>([
-		{
-			id: new Date().getTime().toString(),
-			name: 'São Paulo',
-		},
-	]);
+interface ListaProps {
+  list: Array<ICity>;
+}
 
-	async function getList() {
-		try {
-			const response = await fetch('/api/cities/10');
-			const data = await response.json();
+export default function Lista({ list }: ListaProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2>Lista de cidades</h2>
 
-			if (!response.ok) throw new Error('Erro ao obter os dados');
+        <div data-list-container>
+          {list?.map((city) => (
+            <div data-list-item key={city.id}>
+              {city.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
-			setUsers(data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+export async function getStaticProps() {
+  try {
+    const apiUrl = process.env.API_URL || "http://localhost:8080";
+    const response = await fetch(`${apiUrl}/api/cities/10`);
+    const data = await response.json();
+    if (response.status !== 200) throw new Error("Erro ao obter os dados");
 
-	useEffect(() => {
-		getList();
-	}, []);
+    return {
+      props: {
+        list: data,
+      },
+      revalidate: 60,
+    };
+  } catch (error) {
+    console.error(error);
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.content}>
-				<h2>Lista de cidades</h2>
-
-				<div data-list-container>
-					{list.map((city) => (
-						<div data-list-item key={city.id}>
-							{city.name}
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+    return {
+      props: {
+        list: [
+          {
+            id: new Date().getTime().toString(),
+            name: "São Paulo",
+          },
+        ],
+      },
+      revalidate: 60,
+    };
+  }
 }
